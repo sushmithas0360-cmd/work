dd71e9df02fb56eddcd5ac5715f27b44b1a7a3d9
Sushmithagowda <sushmithas0360@gmail.com>
2026-06-04 14:46:39 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Startup Analytics Dashboard",
+    page_icon="🚀",
+    layout="wide"
+)
+
+st.title("🚀 Startup Analytics Dashboard")
+
+st.markdown("""
+### Features
+
+✔ Startup Overview
+
+✔ Industry Analysis
+
+✔ Regional Insights
+
+✔ Profitability Analytics
+
+✔ Funding Analytics
+
+✔ Predictive Analytics
+
+✔ Interactive Charts
+""")
